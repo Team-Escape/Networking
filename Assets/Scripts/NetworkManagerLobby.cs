d0396cde81790fed2e9527aee75a83e3daec2e8c
AaronXue0 <her8153101@yahoo.com.tw>
2021-07-07 05:32:40 +0800

    Add networkmanager

---
+++ Assets/Scripts/NetworkManagerLobby.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mirror.Game.Lobby
+{
+    public class NetworkManagerLobby : NetworkManager
+    {
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+        }
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+        }
+
+        public override void OnStopServer()
+        {
+            base.OnStopServer();
+        }
+
+        public override void OnStopClient()
+        {
+            base.OnStopClient();
+        }
+
+        public override void OnClientConnect(NetworkConnection conn)
+        {
+            base.OnClientConnect(conn);
+        }
+
+        public override void OnClientDisconnect(NetworkConnection conn)
+        {
+            base.OnClientDisconnect(conn);
+        }
+
+    }
+}
