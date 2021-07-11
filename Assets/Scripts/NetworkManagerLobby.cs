b66b7ea519c33e31a386cb83b27b3eec7b47566f
AaronXue0 <her8153101@yahoo.com.tw>
2021-07-11 16:19:08 +0800

    Update with ServerChangeScene

--- Assets/Scripts/NetworkManagerLobby.cs
+++ Assets/Scripts/NetworkManagerLobby.cs
@@ -21,7 +21,7 @@ namespace Mirror.EscapeGame
 
         string gameScene = "";
 
-        public void ChangeScene(string name) => SceneManager.LoadSceneAsync(name);
+        public void ChangeScene(string name) => ServerChangeScene(name);
 
         void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
